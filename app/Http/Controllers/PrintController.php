86ec265845a3cf3513092be10dd40e2b0c80e4b6
VijayKadiyam <freelancervijayk@gmail.com>
2018-08-07 17:45:28 +0530

    Print Test Commit 1

--- app/Http/Controllers/PrintController.php
+++ app/Http/Controllers/PrintController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Mike42\Escpos\PrintConnectors\CupsPrintConnector;
+use Mike42\Escpos\PrintConnectors\WindowsPrintConnector;
 use Mike42\Escpos\Printer;
 
 class PrintController extends Controller
 {
   public function index()
   {
-    $connector = new CupsPrintConnector("EPSON_TM_T82_S_A");
+    // $connector = new CupsPrintConnector("EPSON_TM_T82_S_A");
+    $connector = new WindowsPrintConnector("Receipt Printer");
     $printer = new Printer($connector);
 
     $printer -> text("Hello World!\n");
