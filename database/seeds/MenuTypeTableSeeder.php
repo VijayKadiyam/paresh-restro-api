876a402206112bafbef07149250ede5562c0987d
VijayKadiyam <freelancervijayk@gmail.com>
2018-08-23 19:46:58 +0530

    Print format changed 5

--- database/seeds/MenuTypeTableSeeder.php
+++ database/seeds/MenuTypeTableSeeder.php
@@ -12,12 +12,20 @@ class MenuTypeTableSeeder extends Seeder
    */
   public function run()
   {
-    MenuType::create(['type'  =>  'Appetizers']);
-    MenuType::create(['type'  =>  'Breakfast']);
-    MenuType::create(['type'  =>  'Continental / Fast Food']);
-    MenuType::create(['type'  =>  'Veg Entree']);
-    MenuType::create(['type'  =>  'Non Veg Entree']);
-    MenuType::create(['type'  =>  'Rice']);
+    // MenuType::create(['type'  =>  'Appetizers']);
+    // MenuType::create(['type'  =>  'Breakfast']);
+    // MenuType::create(['type'  =>  'Continental / Fast Food']);
+    // MenuType::create(['type'  =>  'Veg Entree']);
+    // MenuType::create(['type'  =>  'Non Veg Entree']);
+    // MenuType::create(['type'  =>  'Rice']);
+    // MenuType::create(['type'  =>  'Extras']);
+    MenuType::create(['type'  =>  'Milkshake']);
+    MenuType::create(['type'  =>  'Pasta']);
+    MenuType::create(['type'  =>  'Pizza']);
+    MenuType::create(['type'  =>  'Dessert']);
+    MenuType::create(['type'  =>  'Chillers']);
+    MenuType::create(['type'  =>  'Sandwiches']);
+    MenuType::create(['type'  =>  'Softies']);
     MenuType::create(['type'  =>  'Extras']);
   }
 }
